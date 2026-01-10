629010c554182d03458656ea007385a5ddbde891
J-Kamrath <kamrath.private@proton.me>
2026-01-10 23:30:45 +0100

    Added a place for future cat facts

--- index.php
+++ index.php
@@ -14,20 +14,27 @@ if (isset($_POST['run'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./reset.css">
     <link rel="stylesheet" href="./style.css">
     <title>Document</title>
 </head>
 
 <body>
-    <div id=main_container>
-        <img src="<?= $data["url"] ?>" width="500px" id="img_container">
-        <h1>Little Kitties</h1>
-        <form method="post">
-            <button type="submit" name="run" value="1">
-                Show more Kitties!
-            </button>
-        </form>
+    <div id="flex_box">
+        <div id=image_container>
+            <img src="<?= $data["url"] ?>" id="img_container">
+            <h1>Little Kitties 🐱</h1>
+            <form method="post">
+                <button type="submit" name="run" value="1">
+                    Show more Kitties! 🐱
+                </button>
+            </form>
+        </div>
+        <div id="text_container">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum asperiores ipsum facere magna Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora illum, quaerat omnis fugit facere, in doloremque fugiat eveniet totam exercitationem debitis alias at ipsa rerum minus aperiam! Rem, neque adipisci.
+        </div>
     </div>
+
 
 </body>
 
